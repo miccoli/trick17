e9c6209fad1948babad71d7e6bda4177ec39887d
Stefano Miccoli <stefano.miccoli@polimi.it>
2023-08-12 21:52:49 +0200

    Better error handling in JournalHandler.emit method
    Emit now calls handleError when an error is encountered.
    See
    <https://docs.python.org/3/library/logging.html#logging.Handler.handleError>
    
    Correct tests.
    
    To fix: implement memfd logging when errno.EMSGSIZE is raised.

--- src/trick17/journal.py
+++ src/trick17/journal.py
@@ -6,6 +6,7 @@ import errno
 import logging
 import os
 import socket
+import stat
 import struct
 import sys
 import syslog
@@ -48,28 +49,42 @@ class JournalHandler(logging.Handler):
         super().__init__()
         self.soxx = socket.socket(family=socket.AF_UNIX, type=socket.SOCK_DGRAM)
         self.soxx.settimeout(None)
+        # check if SADDR is accessible and a socket
+        if not os.access(self.SADDR, os.F_OK):
+            msg = f"Nonexistent journal socket '{self.SADDR}'"
+            raise RuntimeError(msg)
+        elif not os.access(self.SADDR, os.W_OK):
+            msg = f"Not writable journal socket '{self.SADDR}'"
+            raise RuntimeError(msg)
+        res = os.stat(self.SADDR)
+        if not stat.S_ISSOCK(res.st_mode):
+            msg = f"'{self.SADDR}' not a socket"
+            raise RuntimeError(msg)
 
     def emit(self, record: logging.LogRecord) -> None:
         """emit record on journald socket"""
 
-        lev = self._log_level(record.levelno)
-        msg = self.format(record)
-        dg: bytes = (
-            self._serialize(b"MESSAGE", msg.encode()) + f"PRIORITY={lev:d}\n"
-            f"LOGGER={record.name}\n"
-            f"THREAD_NAME={record.threadName}\n"
-            f"PROCESS_NAME={record.processName}\n"
-            f"CODE_FILE={record.pathname}\n"
-            f"CODE_LINE={record.lineno}\n"
-            f"CODE_FUNC={record.funcName}\n".encode()
-        )
         try:
-            nsent = self.soxx.sendto(dg, self.SADDR)
-        except OSError as err:
-            if err.errno == errno.EMSGSIZE:
-                # FIXME: implement alternative protocol
-                errmsg = "Log record to long, but fd logging not implemented"
-                raise NotImplementedError(errmsg) from err
-            else:
-                raise
-        assert nsent == len(dg), f"Boundary broken? {nsent} != {len(dg)}"
+            lev = self._log_level(record.levelno)
+            msg = self.format(record)
+            dg: bytes = (
+                self._serialize(b"MESSAGE", msg.encode()) + f"PRIORITY={lev:d}\n"
+                f"LOGGER={record.name}\n"
+                f"THREAD_NAME={record.threadName}\n"
+                f"PROCESS_NAME={record.processName}\n"
+                f"CODE_FILE={record.pathname}\n"
+                f"CODE_LINE={record.lineno}\n"
+                f"CODE_FUNC={record.funcName}\n".encode()
+            )
+            try:
+                nsent = self.soxx.sendto(dg, self.SADDR)
+            except OSError as err:
+                if err.errno == errno.EMSGSIZE:
+                    # FIXME: implement alternative protocol
+                    errmsg = "Log record to long, but fd logging not implemented"
+                    raise NotImplementedError(errmsg) from err
+                else:
+                    raise
+            assert nsent == len(dg), f"Boundary broken? {nsent} != {len(dg)}"
+        except Exception:
+            self.handleError(record)
--- tests/test_journal.py
+++ tests/test_journal.py
@@ -6,6 +6,29 @@ import pytest
 from trick17 import journal
 
 
+def test_broken_handler(tmp_path):
+    """Test error returns when JournalHandler is instantiated but
+    JournalHandler.SADRR points to an invalid socket.
+
+    Instead of monkey patching JournalHandler we subclass it locally with
+    BrokenHandler, and test the given error conditions"""
+    test_path = tmp_path / "socket"
+
+    class BrokenHandler(journal.JournalHandler):
+        SADDR = str(test_path)
+
+    with pytest.raises(RuntimeError, match="^Nonexistent journal socket"):
+        BrokenHandler()
+
+    test_path.touch()
+    with pytest.raises(RuntimeError, match="not a socket$"):
+        BrokenHandler()
+
+    test_path.chmod(0)
+    with pytest.raises(RuntimeError, match="^Not writable journal socket"):
+        BrokenHandler()
+
+
 def test_handler():
     handler = journal.JournalHandler()
 
@@ -22,12 +45,8 @@ def test_handler():
 
     root.info("Multiline\nmessage")
 
-    with pytest.raises(RuntimeError):
-        root.info("A" * 2**18)
-
-    handler.SADDR = "/notexistent"
-    with pytest.raises(FileNotFoundError):
-        root.info("test")
+    # FIXME: check big message
+    # root.info("A" * 2**18)
 
 
 def test_level():
